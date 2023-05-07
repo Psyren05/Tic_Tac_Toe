e2e9323fcd39e4ee9fbebbb782a4d332366aaa98
Lishan <lishan.low11@gmail.com>
2023-05-07 16:39:07 +0100

    added more functions

--- index.js
+++ index.js
@@ -20,20 +20,39 @@ initializeGame()
 function initializeGame() {
     cells.forEach(cell => cell.addEventListener("click", cellClicked))
     restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
 }
 function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
 
 }
 function updateCell(cell, index){
-
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 }
 function changePlayer(){
+    currentPlayer = ( currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 
 }
 
 function checkWinner(){
+    let roundWon = false;
 
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+    }
 }
 function restartGame(){
 
